8346ea5a5ea37737fb6dbc06bbdf8c43075e4810
Snoussi Youssef <snyoussef07@gmail.com>
2020-12-02 16:57:47 +0100

    appelle de cinema service dans ngOnInit dans cinema component

--- src/app/cinema/cinema/cinema.component.ts
+++ src/app/cinema/cinema/cinema.component.ts
@@ -7,11 +7,19 @@ import { CinemaService } from 'src/app/services/cinema.service';
   styleUrls: ['./cinema.component.scss'],
 })
 export class CinemaComponent implements OnInit {
-
+  rooms: any;
+  public hostPicture: string = 'http://localhost:8080/movies/pictures/';
 
   constructor(private cinemaService: CinemaService) {}
 
   ngOnInit(): void {
-
+    this.cinemaService.getRooms().subscribe(
+      (data) => {
+        this.rooms = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
