b615f3df8c75f6ff75eaff17e66a0802b1ecc475
Snoussi Youssef <snyoussef07@gmail.com>
2020-12-02 11:43:28 +0100

    ajout de httpClientModule et du component cinema

---
+++ src/app/cinema/cinema/cinema.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cinema',
+  templateUrl: './cinema.component.html',
+  styleUrls: ['./cinema.component.scss']
+})
+export class CinemaComponent implements OnInit {
+
+  public rooms:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+}
