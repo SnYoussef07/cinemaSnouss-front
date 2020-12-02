fe929f31a9afadedcee584890fce0348bcf83448
Snoussi Youssef <snyoussef07@gmail.com>
2020-12-02 11:45:33 +0100

    creation de cinemaService et inhection dans cinema component

--- src/app/cinema/cinema/cinema.component.ts
+++ src/app/cinema/cinema/cinema.component.ts
@@ -1,19 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CinemaService } from 'src/app/services/cinema.service';
 
 @Component({
   selector: 'app-cinema',
   templateUrl: './cinema.component.html',
-  styleUrls: ['./cinema.component.scss']
+  styleUrls: ['./cinema.component.scss'],
 })
 export class CinemaComponent implements OnInit {
+  public rooms: any;
 
-  public rooms:any;
+  constructor(private cinemaService: CinemaService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-
-
+  ngOnInit(): void {}
 }
