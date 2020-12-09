6d9f0db526b32155ae8aca0f2842bb9180420122
Snoussi Youssef <snyoussef07@gmail.com>
2020-12-09 11:27:43 +0100

    import du formBuilder

--- src/app/component/admin-movies/admin-movies.component.ts
+++ src/app/component/admin-movies/admin-movies.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-movies',
   templateUrl: './admin-movies.component.html',
-  styleUrls: ['./admin-movies.component.scss']
+  styleUrls: ['./admin-movies.component.scss'],
 })
 export class AdminMoviesComponent implements OnInit {
+  public movieForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.movieForm = this.fb.group({});
   }
 
+  ngOnInit(): void {
+    this.movieForm = this.fb.group({});
+  }
+
+  public onAddMovie() {
+    console.log(this.movieForm.value);
+  }
 }
