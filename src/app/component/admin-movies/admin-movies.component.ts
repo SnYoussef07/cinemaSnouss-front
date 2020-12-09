6803c7c37193d88abb6be29ea518f5f246f39338
Snoussi Youssef <snyoussef07@gmail.com>
2020-12-10 00:25:14 +0100

    ajout de service upload picture et banner

--- src/app/component/admin-movies/admin-movies.component.ts
+++ src/app/component/admin-movies/admin-movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AdminMoviesService } from 'src/app/services/admin-movies.service';
 
 @Component({
   selector: 'app-admin-movies',
@@ -9,7 +10,10 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class AdminMoviesComponent implements OnInit {
   public movieForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private adminMovieService: AdminMoviesService
+  ) {
     this.movieForm = this.fb.group({});
   }
 
@@ -21,10 +25,53 @@ export class AdminMoviesComponent implements OnInit {
       duration: [],
       releaseDate: [],
       trailer: [],
+      picture: [],
+      banner: [],
+      fileSource: [],
+      fileSourceBanner: [],
     });
   }
 
   public onAddMovie() {
+    this.adminMovieService.addMovie(this.movieForm.value).subscribe(
+      (data: any) => {
+        this.adminMovieService
+          .uploadPictures(this.movieForm.value, data.id)
+          .subscribe(
+            (item: any) => {
+              this.adminMovieService
+                .uploadBanner(this.movieForm.value, data.id)
+                .subscribe((data: any) => {
+                  console.log('success');
+                });
+            },
+            (err: any) => {
+              console.log(err);
+            }
+          );
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
     console.log(this.movieForm.value);
+  }
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+
+      this.movieForm.patchValue({
+        fileSource: file,
+      });
+    }
+  }
+  onFileChangeBanner(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+
+      this.movieForm.patchValue({
+        fileSourceBanner: file,
+      });
+    }
   }
 }
--- src/app/services/admin-movies.service.ts
+++ src/app/services/admin-movies.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
@@ -7,7 +8,26 @@ import { FormGroup } from '@angular/forms';
 export class AdminMoviesService {
   public host: string = 'http://localhost:8080';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  public addMovie(movieForm: FormGroup) {}
+  public addMovie(movieForm: any) {
+    let formdata: FormData = new FormData();
+    formdata.append('file', movieForm.fileSource);
+
+    //let data = { movieForm: movieForm, formdata: formdata };
+
+    return this.http.post(`${this.host}/movies`, movieForm);
+  }
+  public uploadPictures(movieForm: any, idMovie: any) {
+    let formdata: FormData = new FormData();
+    formdata.append('file', movieForm.fileSource);
+
+    return this.http.post(`${this.host}/movies/uploadPictures/${idMovie}`, formdata);
+  }
+  public uploadBanner(movieForm: any, idMovie: any) {
+    let formdata: FormData = new FormData();
+    formdata.append('file', movieForm.fileSourceBanner);
+
+    return this.http.post(`${this.host}/movies/uploadBanner/${idMovie}`, formdata);
+  }
 }
