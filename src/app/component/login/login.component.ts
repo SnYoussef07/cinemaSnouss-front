430bb350bdaf8106b8abcbb9556d509a7dc19a51
Snoussi Youssef <snyoussef07@gmail.com>
2020-12-16 19:27:43 +0100

    ajout du service login et recuperation du jwt token sans le stocker

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { LoginServiceService } from 'src/app/services/login-service.service';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
+  public mode: number = 0;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private loginService: LoginServiceService
+  ) {
     this.loginForm = this.fb.group({});
   }
 
@@ -22,5 +27,15 @@ export class LoginComponent implements OnInit {
 
   public onLogin() {
     console.log(this.loginForm.value);
+    this.loginService.login(this.loginForm.value).subscribe(
+      (data: any) => {
+        console.log('success');
+        let jwtToken = data.headers.get('Authorization');
+        console.log(jwtToken);
+      },
+      (err: any) => {
+        this.mode = 1;
+      }
+    );
   }
 }
---
+++ src/app/services/login-service.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginServiceService {
+  public host: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  login(user: any) {
+    /* on veux pas recuperer json mais le http */
+    return this.http.post(`${this.host}/login`, user, { observe: 'response' });
+  }
+}
